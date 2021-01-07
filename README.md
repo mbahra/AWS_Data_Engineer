ad611ef4e749b6c61efa29f0432e9fbc668a6522
mbahra <bahra.mehdi@gmail.com>
2021-01-07 14:33:36 +0100

    README test

--- README.md
+++ README.md
@@ -12,7 +12,33 @@ To do this, we will create our datalake, where we will store and process all of 
 
 To avoid costs and because my purpose is focused on the engineering part, the analysis part will be very restricted.
 It will consist in observing the correlation between some basic stats (goals, shots, etc) and the attractiveness of a match based on a sentimental analysis of tweets.
-I will focus on english Barclays Premier League, considering all the teams and matchweeks.
+I will focus on english Barclays Premier League, considering all the teams and matchweeks for the current season (2020/2021).
+
+## AWS account
+
+To begin, we have to create an AWS account at this link :
+https://portal.aws.amazon.com/billing/signup#/start
+
+Pay attention to the pricing conditions. The AWS Free Tier conditions are provided here :
+https://aws.amazon.com/free/?nc1=h_ls&all-free-tier.sort-by=item.additionalFields.SortRank&all-free-tier.sort-order=asc
+
+We can opt in to or out of the AWS Free Tier usage alerts through the Billing and Cost Management console.
+Sign in to the AWS Management Console and open the Billing and Cost Management console at
+https://console.aws.amazon.com/billing/.
+Under Preferences in the navigation pane, choose Billing preferences.
+Under Cost Management Preferences, select Receive AWS Free Tier Usage Alerts to opt in to Free Tier usage alerts. To opt out, clear the Receive AWS Free Tier Usage Alerts check box.
+
+## Installation
+
+All this part is available on https://realpython.com/python-boto3-aws-s3/.
+
+Install the Boto3 SDK on your computer with the following command in your terminal :
+$ pip install boto3
+
+To make it run against your AWS account, you’ll need to provide some valid credentials.
+If you already have an IAM user that has full permissions to S3, you can use those user’s credentials (their access key and their secret access key) without needing to create a new user.
+Otherwise, the easiest way to do this is to create a new AWS user and then store the new credentials.
+
 
 ## Data
 
--- api_calls.py
+++ api_calls.py
@@ -1,7 +1,16 @@
 import requests
 import datetime
 import json
+import schedule
+import time
+import logging
+import boto3
+from botocore.exceptions import ClientError
 
+todayDate = datetime.datetime.today().strftime('%Y-%m-%d')
+previousWeekDate = (datetime.datetime.today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
+
+#! handle request errors
 def fixturesRequest():
 
     url = "https://api-football-beta.p.rapidapi.com/fixtures"
@@ -11,16 +20,13 @@ def fixturesRequest():
     'x-rapidapi-host': "api-football-beta.p.rapidapi.com"
     }
 
-    todayDate = datetime.datetime.today().strftime('%Y-%m-%d')
-    previousWeek = datetime.datetime.today() - datetime.timedelta(days=7)
-    previousWeekDate = previousWeek.strftime('%Y-%m-%d')
-
     querystring = {"league":"39", "season":"2020", "from":previousWeekDate, "to":todayDate}
 
     response = requests.request("GET", url, headers=headers, params=querystring)
 
     return response.json()
 
+#! handle request errors
 def statisticsRequest(fixtureId):
 
     url = "https://api-football-beta.p.rapidapi.com/fixtures/statistics/fixture/" + fixtureId + "/"
@@ -33,3 +39,44 @@ def statisticsRequest(fixtureId):
     response = requests.request("GET", url, headers=headers)
 
     return response.json()
+
+#! add S3 connexion
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    if object_name is None:
+        object_name = file_name
+
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+def apiFootballRequest():
+
+    #! connexion to S3 bucket
+
+    fixturesJson = fixturesRequest()
+    fixturesJsonName = "fixturesJson_" + todayDate
+    upload_file(fixturesJson, bucket, fixturesJsonName) #! specify your bucket's name
+
+    for fixtureId in fixturesJson:  #! write with the correct json syntax
+        statistiscsJson = statisticsRequest(fixtureId)
+        statistiscsJsonName = "statiscsJson_" + fixtureId
+        upload_file(statistiscsJson, bucket, statistiscsJsonName)   #! specify your bucket's name
+
+schedule.every.tuesday.at("08:00").do(apiFootballRequest)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
